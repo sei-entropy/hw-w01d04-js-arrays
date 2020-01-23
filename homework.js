a5898d8d2e2e280d4c073098b435d8380626241a
aLi <aaljamid@emich.edu>
2020-01-23 09:30:51 -0500

    homework 4

--- homework.js
+++ homework.js
@@ -65,9 +65,80 @@ const addDashes = function(arr) {
 const countUpAndDown = function(str) {
   let countString = "";
 
-  for (let i = 0; i < str.length; i++) {
-    console.log(i);
+  for (let i = 1; i < str; i++) {
+    countString += String(i);
   }
 
-  //return countString;
+  for (let i = str; i > 0; i--) {
+    countString += String(i);
+  }
+
+  return countString;
 };
+
+/// 7
+
+const wordsWithA = function(arr) {
+  let result = [];
+  let j = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].indexOf("a") !== -1) {
+      result[j++] = arr[i];
+    }
+  }
+  return result;
+};
+
+/////// 8
+
+const wordsWithLetter = function(letter, array) {
+  let result = [];
+  let n = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].indexOf(letter) !== -1) {
+      result[n++] = array[i];
+    }
+  }
+  return result;
+};
+
+wordsWithLetter("g", ["cat", "rabbit", "dog", "frog"]);
+
+//// 9
+
+const longestWord = function(str) {
+  let array = str.split(" ");
+
+  let largestWordLength = array[0].length;
+  let largestWordIndex = 0;
+  for (let i = 1; i < array.length; i++) {
+    if (array[i].length > largestWordLength) {
+      largestWordLength = array[i].length;
+
+      largestWordIndex = i;
+    }
+  }
+  return array[largestWordIndex];
+};
+
+longestWord("The cat in the house");
+
+/// 10
+const largestEvenNumber = function(array) {
+  let even = [];
+
+  let n = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) even[n++] = array[i];
+  }
+  let largestEven = even[0];
+  for (let i = 0; i < even.length; i++) {
+    if (largestEven < even[i]) largestEven = even[i];
+  }
+  return largestEven;
+};
+
+largestEvenNumber([1, 2, 3, 10, 4, 7, 0]);
