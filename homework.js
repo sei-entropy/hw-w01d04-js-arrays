67a8a08ad626fc0d65219ae209aa10dd66aacbe8
huda <you@example.com>
2020-01-27 21:38:49 +0300

    answer all the homework

--- homework.js
+++ homework.js
@@ -1,89 +1,86 @@
 
-const summation = function(n)
-
-{
-let sum = 0;
-
-for (let i = 1; i <= n; i=i+1) {
-
-    sum += i;//1  //3 //6 //
-}
+function summation(num) {
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+      sum += i;
+    }
     return sum;
-
-}
+  }
 //summation(5);15
 
 //2.....................................................
-const summationEven = function (n)
-{
-    let summation = 0;
-
-    for (let i = 0; i < n ; i=i+1) {
-
-        if ( i %2 ===0){
-
-         summation += i;
-
-} return summation;
-}
+function summationEven(num) {
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+      if (!(i % 2)) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
 // summationEven(5)
-//6
-//3.......................................
-const avg = function (arry){
-    let summation = 0;
-    for (let i = 0; i < arry.length; i=i+1) {
 
-        summation = summation + arry[i];
-       summation= summation/arry.length;
-} return summation;
-}
+//3.......................................
+function avg(arr) {
+    let result = 0;
+    for (let i = 0; i < arr.length; i++) {
+      result += arr[i];
+    }
+    return (result = result / arr.length);
+  }
 //[8, 2, 2, 4] = 4
 
 
 
 //4....................................................
+function reverse(word) {
+    let reversedWord = "";
+    for (i = word.length - 1; i >= 0; i--) {
+      reversedWord += word[i];
+    }
+    return reversedWord;
+  }
 
-function reversWord(w) {
-
-var newString = "";
-for (let i = w.length - 1; i >= 0; i--) {
-newString += w[i];
-}
-return newString;
-}
-
-reversWord('caterpillar');
+//reversWord('caterpillar');
 
 //5..........................................
 
-const test  = function (arry){
-    let n = "";
+const addDashes  = function (arry){
+    let word = "";
 
     for (let i = 0; i < arry.length; i=i+1) {
-       n= arry.join('-');
+       word= arry.join('-');
     }
-    return n;}
+    return word;}
 //test(['test1','test2','test3'])
 //"test1-test2-test3"
 
 }
 //6.....................................
-
-
+function countUpAndDown(num) {
+    let count = "";
+    for (let i = 1; i <= num; i++) {
+      count += i + " ";
+    }
+    for (let i = num - 1; i > 0; i--) {
+      count += i + " ";
+    }
+    return count;}
+    //
 //7.................................
 
-const names1 = function (word){
-
-    let w=[]; 
-    
-    for (let i = 0 ; i<word.length;i++)
-    {
-    if(word[i].indexOf('a') > -1){
-    w.push(word[i]);
+function wordsWithA(arr) {
+    newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = arr[i].toLowerCase();
+      for (let j = 0; j < arr[i].length; j++) {
+        if (arr[i][j] == "a") {
+          newArr.push(arr[i]);
+        }
+      }
     }
-    }console.log(w);
-        
-    }
+    return newArr;
+  }
 
 //names1(['cat', 'rabbit', 'dog', 'frog'])
 // ["cat", "rabbit"]
@@ -99,28 +96,37 @@ const wordsWithLetter = function (w,word){
     if(word[i].indexOf(w) > -1){
     w.push(word[i]);
     }
-    }console.log(w);
-        
-    }
-
+    }return w; }
+    //wordsWithLetter("g", ["cat", "rabbit", "dog", "frog"])
+// ["dog", "frog"]
 
 //9..................................
 
+function longestWord(word) {
+    word = word.split(" ");
+    let longest = word[0];
+    for (let i = 0; i < word.length; i++) {
+      if (word[i].length > longest.length) {
+        longest = word[i];
+      }
+    }
+    return longest;
+  }//longestWord("The cat in the house")
+
 //10.......................
 
-const num = function(arry){
+function largestEvenNumber(arr) {
 
-    let largestEvenNumber = 0;
-   
-    for (let i = 0; i <  num.length; i++) {
-   
-       if (num[i] % 2 === 0 )
-   {
-   if(num[i] > largestEvenNumber){
-       largestEvenNumber = num[i] }
-      
+    let num = 0;
+
+    for (let i = 0; i < arr.length; i++) 
+    {
+      if (!(arr[i] % 2) && arr[i] > num)
+       {
+        num = arr[i];
+      }
     }
-    return largestEvenNumber;
+    return num;
+  }
+  //num([1,2,3,10,4,7,0])
    
-   }};//num([1,2,3,10,4,7,0])
-   //0
