b619a4545415f5d9182da4351116e5a14ef046fa
Amirah Almani <amirahalmani@gmail.com>
2020-01-28 22:29:06 +0300

    Final Update

--- homework.js
+++ homework.js
@@ -1,61 +1,50 @@
 //1-----------------
-let sum = [1, 2, 3, 4, 5].reduce(add,0); 
-
-function add(sum, a) {
-    return sum + a;
-}
-
-console.log(sum);
-
+const sum = function (num){
+  let result=0;
+    for (let y =0 ; y<=num ; y++){
+    
+    result=num+y;
+    result+=y
+   
+    }
+   console.log(result);
+    }
+  sum(5)
+  
 
 
 //2------------------------------------------------------
-let sum = [1, 2, 3, 4, 5];
-let num1=0;
-let num2=0;
-let add=0;
 
 
-for (let x=0 ; x<sum.length ; x++){
+const summationEven = function(num){
 
+  let sum=0
 
+  for(let i= 1 ; i<=num; i++){
 
-if ((sum[x]%2)==0){
+      if(i % 2===0){
 
- num1=sum[x];
-
-
-}}
-
-for (let x=0 ; x<sum.length ; x++){
-
-if (num1 !== sum[x] && (sum[x]%2)==0){
-
- num2=sum[x];
-
-
-}}
-
- function myFunc(sum) {
-  return num1+num2;;
+sum +=i;
+      }}
+  return sum;
 }
 
+summationEven(5)
 
-myFunc(sum);
+
 
 
 //3-------------------------------------------------------------
 
 
-const avg=[8,2,2,4]
-function numAvg(num){
-    var total = 0;
-    for(var i = 0;i < num.length; i++) { 
-        total+=num[i];
-    }
-    return total/num.length;
+function Avg(num){
+  var total = 0;
+  for(let i = 0;i < num.length; i++) { 
+      total+=num[i];
+  }
+  return total/num.length;
 }
-numAvg(avg);
+Avg([8,2,2,4]);
 
 
 
@@ -63,90 +52,110 @@ numAvg(avg);
 
 //4=------------------------------------------------------------
 
-let txt='caterpillar'
+let result = null;
 
-txt.split("").reverse().join("")
+const reverse = function(txt){
 
+result = txt.split("").reverse().join("");
+return result;
 
+}
+
+reverse("caterpillar")
 
 
 //5-----------------------------------------------------------
 
 
-const addDashes=['test1' ,'teat2', 'test3']
-console.log(addDashes.join("-" ));
+const addDashes = function(array){
 
+  return array.join("-");  
+      }
+
+      addDashes(['test1', 'test2', 'test3'])
 
 
 //6 ----------------------------------------------------------
 
-const countUpAndDown=[1,2,3];
-
-for (let x=0 ; x<countUpAndDown.length ; x++){
-
-console.log(countUpAndDown[x]);
 
 
-}
+const countUpAndDown = function(num){
+
+  for(let i = 1; i<=num;i++){
+
+      console.log(i);
+
+  }
+
+  for(let x = num-1 ; x > 0 ; x--){
 
 
-function revers(countUpAndDown) {
+      console.log(x);
+  } }
 
-countUpAndDown.pop();
-countUpAndDown.reverse();
-console.log(countUpAndDown);
-
-
-}
-
-revers(countUpAndDown);
+countUpAndDown(3);
 
 
 //7---------------------------------------------------------------
 
-const wordsWithA = ['cat', 'rabbit', 'dog', 'frog'];
+function wordsWithA(words) {
+  const word = [];
+
+  words.forEach(function(elem, index) {
 
 
-for (let x=0 ; x<wordsWithA.length ; x++){
- 
-let result = wordsWithA.find(function(element) {
-  return element >= 'a';
-});
-
+   if (elem.includes("a")) {
+      word.push(elem);
+    }
+  });
+  console.log(word);
 }
-console.log(result);
 
-
-
+wordsWithA(["cat", "rabbit", "dog", "frog"]);
 
 //8-------------------------------------------------------------
-const wordsWithLetter = ['cat', 'rabbit', 'dog', 'frog'];
 
 
-for (let x=0 ; x<wordsWithLetter.length ; x++){
- 
-let result = wordsWithLetter.find(function(element) {
-  return element >= 'g';
-});
 
+function wordsWithLetter(letter, words) {
+  const word = [];
+
+  words.forEach(function(elem, index) {
+    if (elem.includes(letter)) {
+      word.push(elem);
+    }
+  });
+  console.log(word);
 }
-console.log(result);
+
+wordsWithLetter("g", ["cat", "rabbit", "dog", "frog"]);
 
 //9----------------------------------------------------------------
 
 
-function longestWord(str) {
-    let abc = str.split(" ");
-    let newStr = [];
-    for (let i = 0; i < abc.length; i++) {
-        
-         newStr.push(abc[i].length);
-    }
-      let neweSTr= newStr.sort(function(x,y){return y -x;}); 	
-    return neweSTr[0];
- }
+function longestWord(string) {
+
+
+  let arr  = string.split(" ");
+  
+
+let  word = '';
+
+ for (let i = 0; i < arr.length; i++)
+{
+
+  let  x = 0;
+
  
- longestWord("The cat in the house");
+      if (i < arr[i].length) {
+          x += arr[i].length;
+          word = arr[i];
+      }
+  }
+  return word;
+}
+ 
+ longestWord("The cat in house the");
 
 
 
@@ -154,8 +163,20 @@ function longestWord(str) {
  //10-------------------------------------------------------------
 
 
- let largestEvenNumber=[1,2,3,10,4,7,0];
-let max = largestEvenNumber.reduce(function(a, b) {
-    return Math.max(a, b);
-});
-console.log(max);
+ function largestEvenNumber(array) {
+
+  let l = 0;
+
+  for (let i = 0; i < array.length; i++) {
+
+    const x = array[i];
+    if (x % 2 === 0) {
+
+      if (x > l) {
+        l = x;
+    }
+    }}
+
+  console.log(l);
+ }
+ largestEvenNumber([1,2,3,10,4,7,0])
