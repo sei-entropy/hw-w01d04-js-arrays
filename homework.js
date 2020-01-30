8782684f200ab057cff08486ce85d425eca37404
Renad-Ahmad <59873078+Renad-Ahmad@users.noreply.github.com>
2020-01-31 02:38:39 +0300

    Update homework.js

--- homework.js
+++ homework.js
@@ -1,94 +1,103 @@
 // 1
-function numberSum(n) {
+function summation(number) {
     let total = 0;
-      for(let i = 1; i <= n; i++){
+      for(let i = 1; i <= number; i++){
         total += i;
       }
       return total;
   }
+summation(5)
 
 // 2
-    function numberEven(n){
+    function summationEven(number){
       let sum = 0;
-      for(let i = 1; i< n; i++) {
+      for(let i = 1; i <= number; i++) {
        if(i % 2 === 0) {
        sum = sum + i;
     }
     }
      return sum;
     }
+    summationEven(5)
 
 // 3
-    function numberAvg(n){
+    function avg(number){
         var sum = 0;
-         for(var i in n) {
-          sum += n[i];
+         for(var i in number) {
+          sum += number[i];
         }
-        var numbersCnt = n.length;
+        let numbersCnt = number.length;
         return (sum / numbersCnt);
     }
+     avg([8, 2, 2, 4])
 
 // 4
-    function reverseString(str) {
+    function reverse(str) {
         let splitString = str.split(""); 
      
         let reverseArray = splitString.reverse(); 
         let joinArray = reverseArray.join("");
             return joinArray; 
     }
+    reverse("caterpillar")
+
 
 // 5
-    function combines('test1', 'test2', 'test3']) {
-        
-        for(let i = 1; i <= addDashes; i++){
-          addDashes.join("-");
-          }
-          return addDashes;
+    
+    
+const addDashes = function (testArray){
+     var stringDashes = '';
+     for (let i=0 ; i < testArray.length ; i++){
+         
+       stringDashes += testArray[i] + '-';
       }
-
-      const addDashes = function ()
-
-      {
-   const testArray=['test1', 'test2', 'test3'];
-    for ( let i=0 ; i < testArray.length ; i++)
-    {
-        console.log(testArray.join("-"));
-    }
-}
+    stringDashes = stringDashes.substring(0, stringDashes.length -1);
+    return stringDashes;
+  }
+   
+  addDashes(['test1', 'test2', 'test3'])
 
 // 6
 const countUpAndDown = function (n){
   count = '';
   for(let i = 1; i < n; i++) {
-    count += i; 
+    count += i + ' ' ; 
 }
 for(let i = n; i >= 1; i--){
-    count += i;
+    count += i + ' ';
 }
-return count; 
+return count + ' '; 
 };
+countUpAndDown(3)
+
 
 // 7
-
 const wordsWithA = function(array){
+   
+  const words = [];
   for(let i = 0; i < array.length; i++) {
     if(array[i].indexOf('a') == true){
-  console.log(array[i]);
+   words.push(array[i]);
   
     }
   }
+    return words;
 };
+wordsWithA(['cat', 'rabbit', 'dog', 'frog'])
 
 // 8
-
 function wordsWithLetter(letter , array) {
-  for(let i = 0; i < array.length; i++) {
-    if(array[i].indexOf(letter) !== -1){
-  console.log(array[i]);
+    
+    const words = [];
+    for(let i = 0; i < array.length; i++) {
+        if(array[i].indexOf(letter) !== -1){
+       words.push(array[i]);
   
     }
   }
+    return words;
 };
+wordsWithLetter("g", ['cat', 'rabbit', 'dog', 'frog'])
 
 // 9
 function longestWord(str) {
@@ -105,15 +114,16 @@ function longestWord(str) {
  }
  return strSplit[count]; 
 }
+longestWord("The cat in the house")
 
 // 10
+function largestEvenNumber(array) {
 
-function largestEvenNumber(arra) {
+  array.sort((num1, num2) => num2 - num1);
 
-  arra.sort((num1, num2) => num2 - num1);
-
-  for (let i = 0; i < arra.length; i++) {
-    if (arra[i] % 2 == 0)
-      return arra[i];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 == 0)
+      return array[i];
     }
   }
+ largestEvenNumber([1,2,3,10,4,7,0])
